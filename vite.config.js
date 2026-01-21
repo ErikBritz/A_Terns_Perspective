478dd3f35e8ff0e023045f58173351dac0861503
ErikBritz <erikbritz1313@gmail.com>
2026-01-21 13:59:00 +0100

    chore: remove vite-plugin-cesium and clean dependencies for ES module build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 const { defineConfig } = require("vite");
-const cesium = require("vite-plugin-cesium").default;
 
 module.exports = defineConfig({
   base: "/A_Terns_Perspective/",
-  plugins: [
-    cesium({
-      injectCesiumScript: false
-    })
-  ],
   server: {
     host: "127.0.0.1",
     port: 5501,
