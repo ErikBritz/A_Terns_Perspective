5ccf0379affea83144ec3cf933658a17c2a1a201
ErikBritz <erikbritz1313@gmail.com>
2026-01-21 13:53:26 +0100

    fix: prevent global Cesium.js injection for Vite/Cesium module compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ const cesium = require("vite-plugin-cesium").default;
 
 module.exports = defineConfig({
   base: "/A_Terns_Perspective/",
-  plugins: [cesium()],
+  plugins: [
+    cesium({
+      injectCesiumScript: false
+    })
+  ],
   server: {
     host: "127.0.0.1",
     port: 5501,
